ec0a57f45e1a14a10ea05d37d8eadfd9719e6b0d
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-02-07 18:21:02 +0200

    add payments ui

--- ui100/core/urls.py
+++ ui100/core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home')
+    path('', views.HomeView.as_view(), name='home'),
+    path('payment', views.PatmentView.as_view(), name='payment')
 ]
--- ui100/core/views.py
+++ ui100/core/views.py
@@ -2,3 +2,7 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "core/home.html"
+
+
+class PatmentView(TemplateView):
+    template_name = "core/payment.html"
